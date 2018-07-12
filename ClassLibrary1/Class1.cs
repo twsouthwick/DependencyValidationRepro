84c0bbe468042954fb80125c66fa70882f872321
Taylor Southwick <tasou@microsoft.com>
2018-07-11 21:52:30 -0700

    Add example

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,5 +4,21 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        public void Test()
+        {
+            var t = new Other.Test();
+            t.TryGet(out _);
+        }
+    }
+}
+namespace Other
+{
+    public class Test
+    {
+        public bool TryGet(out int i)
+        {
+            i = 5;
+            return true;
+        }
     }
 }
